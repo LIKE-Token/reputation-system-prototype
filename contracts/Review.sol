86a8da2c3294483834d7848862de58df30fe1baa
maikotrindade <maikocharles@gmail.com>
2018-07-06 13:08:27 -0300

    add current address to Review contract

--- contracts/Review.sol
+++ contracts/Review.sol
@@ -4,14 +4,16 @@ contract Review {
 
     string comment;
     uint rating;
+    address currentAddress;
 
    function setReview(string _comment, uint _rating) public {
        comment = _comment;
        rating = _rating;
+       currentAddress = msg.sender;
    }
    
-   function getReview() view public returns (string, uint) {
-       return (comment, rating);
+   function getReview() view public returns (string, uint, address) {
+       return (comment, rating, currentAddress);
    }
    
 
