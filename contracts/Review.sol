b0f0ec5aa81afa5a9bd19d4109f36bdc3bbace58
Maiko Trindade <mt@ubby.io>
2018-07-10 10:29:34 -0300

    add event 'reviewPerformed'

--- contracts/Review.sol
+++ contracts/Review.sol
@@ -6,15 +6,20 @@ contract Review {
     uint rating;
     address currentAddress;
 
-   function setReview(string _comment, uint _rating) public {
-       comment = _comment;
-       rating = _rating;
-       currentAddress = msg.sender;
-   }
-   
-   function getReview() view public returns (string, uint, address) {
-       return (comment, rating, currentAddress);
-   }
-   
+    event ReviewPerformed (
+        string comment,
+        uint rating
+    );
 
+    function setReview(string _comment, uint _rating) public {
+        comment = _comment;
+        rating = _rating;
+        currentAddress = msg.sender;
+        emit ReviewPerformed(comment, rating);
+    }
+   
+    function getReview() view public returns (string, uint, address) {
+        return (comment, rating, currentAddress);
+    }
+   
 }
