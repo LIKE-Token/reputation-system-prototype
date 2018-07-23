74f29fb59f15c9a5e312aa529cc49866a2ae0546
Maiko Trindade <mt@ubby.io>
2018-07-23 09:14:15 -0300

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Review = artifacts.require("./Review.sol");
+var Testnet = artifacts.require("./ReputationTestnet.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Review);
+  deployer.deploy(Testnet);
 };
