bbfd8d0f9e2692c5526aed70471302d222935376
maikotrindade <maikocharles@gmail.com>
2018-07-24 13:01:41 -0300

    add ico solidity impl and its front-end integration

---
+++ contracts/LikeToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract LikeToken is StandardToken {
+
+    string public name = "LikeToken";
+    string public symbol = "LKE";
+    uint8 public decimals = 3;
+    uint public INITIAL_SUPPLY = 1000000;
+
+    constructor() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
+
+    // function sayHello() public pure returns (string) {
+    //     return "hello LIKE!";
+    // }
+
+}
